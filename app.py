e73fc5ea9d5c7ba7f471e512dc9d981610be2bdd
Satyam Wagh <satyamwagh2006@gmail.com>
2026-08-21 10:47:48 +0530

    Analysing the steps

--- app.py
+++ app.py
@@ -1,51 +1,46 @@
-import joblib
-test= [
-    7,
-    338,
-    "Salaried",
-    34,
-    "Married",
-    2,
-    544644,
-    0,
-    0.0441,
-    18015,
-    6333,
-    30489,
-    12,
-    "Education",
-    "Rural",
-    "Not Graduate",
-    "nan",
-    "MNC"
-]
-label_encoder = joblib.load("label_encoder.pkl")
-onehot_encoder = joblib.load("onehot_encoder.pkl")
-scaler = joblib.load("scaler.pkl")
-model = joblib.load("logistic_regression_model.pkl")
+import joblib as jb
+import warnings
 
-# 1. Label Encoding
-test[-3] = label_encoder.transform(
-    [test[-3]]
+warnings.filterwarnings(
+    "ignore",
+    message="X does not have valid feature names"
 )
-print(test[-3])
-# Updated cols list using integer indices instead of string names
-cols = [
-    2,   # "Employment_Status"
-    4,   # "Marital_Status"
-    13,  # "Loan_Purpose"
-    14,  # "Property_Area"
-    16,  # "Gender"
-    17   # "Employer_Category"
+#step 1:import models
+scaler = jb.load("scaler.pkl")
+model=jb.load("logistic_regression_model.pkl")
+test = [
+    322,
+    235,
+    2345,
+    245,
+    245,
+    245,
+    245,
+    245,
+    245,
+    0,
+    0,
+    1,
+    0,
+    1,
+    1,
+    0,
+    0,
+    0,
+    1,
+    0,
+    1,
+    1,
+    0,
+    0,
+    0,
+    425,
+    245
 ]
+#step 2:scale the values
+scale_values = scaler.transform([test])
 
-test_values = [[]]
-for i in cols:
-    test_values[0].append(test[i])
-    test[i]=True
-print(test)
-print(test_values)
-#2 Transform using the 2D list
-encoded = onehot_encoder.transform(test_values)
-print(encoded)
-#3  Combine numerical + encoded columns
+print(scale_values)
+#step 3:Predict the result
+result=model.predict(scale_values)
+print(result)
